80c569e7e90916681d96b58a9bf5285cfbcd1264
Daniel Ma <danielpluvia@outlook.com>
2019-03-18 21:24:07 +0800

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/DPPopupView/DPPopupView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DPPopupView : NSObject
+@end
+@implementation PodsDummy_DPPopupView
+@end
---
+++ Example/Pods/Target Support Files/Pods-DPPopupView_Tests/Pods-DPPopupView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DPPopupView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DPPopupView_Tests
+@end
